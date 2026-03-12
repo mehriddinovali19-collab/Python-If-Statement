25a582304dc6f62cbb31672be37c0daa655c9b1a
ali mekhridinov <mehriddinovali19@gmail.com>
2026-03-12 17:05:33 +0500

    task 02 bajarildi

---
+++ task02.py
@@ -0,0 +1,10 @@
+number1 = input("enter a number:")
+number2 = input("enter a number:")
+if number1 > number2:
+    print("this is bigger: ", number1)
+elif number1 < number2:
+
+    print("this is bigeger: ",number2)
+
+else:
+    print("equal!")
